05bed6b1f6ba6b10054be7f0dd67cd08357990f6
Evans A. Stepanov <evans@stepanov.email>
2025-03-29 11:28:39 -0400

    feat: enhance logging in API by introducing loggerFactory for configurable pretty logging; update package.json to include pino-pretty and modify frontend title for clarity

--- apps/api/src/utils/logger.ts
+++ apps/api/src/utils/logger.ts
@@ -1,6 +1,16 @@
 import pino from 'pino'
 
-export const appLogger = pino({
+export const loggerFactory = ({
+    prettyLogs
+}: {
+    prettyLogs: boolean
+}) => pino({
+    transport: prettyLogs ? {
+        target: 'pino-pretty',
+        options: {
+            colorize: true
+        }
+    } : undefined,
     formatters: {
         level: (label) => {
             return {
@@ -10,8 +20,10 @@ export const appLogger = pino({
         bindings: () => ({})
     },
     timestamp: pino.stdTimeFunctions.isoTime,
+})
 
-    
+export const appLogger = loggerFactory({
+    prettyLogs: process.env.NODE_ENV === "development"
 })
 
 export type AppLogger = typeof appLogger
